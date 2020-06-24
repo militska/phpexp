058e538e2608b271e5844a7b58440b83fccddb00
militska <militska@intersvyaz.net>
2020-06-24 09:53:05 +0500

    amphp+ недороутинн.пока что мне не нравится автоподгрузка классов

--- web/controllers/ActionPost.php
+++ web/controllers/ActionPost.php
@@ -2,10 +2,43 @@
 
 namespace web\controllers;
 
+
+use mysqli;
+use PDO;
+
 class ActionPost {
 
-    public function __toString()
+
+    public function OpenCon()
     {
-        return "hello from actionPost";
+        $dbhost = "db";
+        $dbuser = "root";
+        $dbpass = "example";
+        $db = "test";
+        $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+        return $conn;
+    }
+
+    function CloseCon($conn)
+    {
+        $conn->close();
+
+    }
+        public function __toString()
+    {
+
+        try {
+
+            var_export($this->OpenCon());
+
+
+        } catch (\Exception $e)
+        {
+            var_export($e);
+        }
+
+
+        return "tr";
     }
 }
